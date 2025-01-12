bf62c19b967e9d1238c6dc4cb0512a7e00da89b3
itsmesamu <143997065+itsmesamu@users.noreply.github.com>
2025-01-12 17:00:05 +0100

    123

--- 2dspiel/plattformer.py
+++ 2dspiel/plattformer.py
@@ -1,4 +1,6 @@
 import arcade.color
+import arcade.color
+import arcade.key
 import pyglet
 pyglet.options["osx_alt_loop"]  = True
 
@@ -6,7 +8,7 @@ import arcade
 
 class Plattformer(arcade.Window):
     def __init__(self):
-        super().__init__(1000,500,"Plattformer")
+        super().__init__(2000,1000,"Plattformer")
 
         arcade.set_background_color(arcade.color.AIR_FORCE_BLUE)
 
@@ -21,8 +23,15 @@ class Plattformer(arcade.Window):
 
         self.szene.get_sprite_list("Tile Layer 1")
 
+        self.szene.get_sprite_list("leiter layer")
+        
+        self.kamera = arcade.Camera(self.width, self.height)
+
+        
+
         self.physik_engine = arcade.PhysicsEnginePlatformer(self.spielfigur, self.szene.get_sprite_list("Tile Layer 1"))
 
+    
     def on_key_press(self,symbol,modifiers):
         if symbol == arcade.key.RIGHT:
             self.spielfigur.change_x = 2
@@ -30,6 +39,13 @@ class Plattformer(arcade.Window):
             self.spielfigur.change_x = -2
         if symbol == arcade.key.SPACE:
             self.spielfigur.change_y = 4
+        if arcade.check_for_collision_with_list(self.spielfigur, self.szene.get_sprite_list("leiter layer")):
+            if symbol == arcade.key.UP:
+                self.spielfigur.change_y = 
+            if symbol == arcade.key.DOWN:
+                self.spielfigur.change_y = -
+        
+
 
     def on_key_release(self,symbol,modifiers):
         if symbol==arcade.key.RIGHT:
@@ -37,15 +53,40 @@ class Plattformer(arcade.Window):
         if symbol == arcade.key.LEFT:
             self.spielfigur.change_x = 0
         if symbol == arcade.key.SPACE:
-            self.spielfigur.change_y=-4 
+            self.spielfigur.change_y=-5
+
+
+    def center_camera_to_player(self):
+        screen_center_x = self.spielfigur.center_x - (self.kamera.viewport_width / 2)
+        screen_center_y= self.spielfigur.center_y - (
+            self.kamera.viewport_height / 2
+        )
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        
+
+        spielfigur_centered = screen_center_x, screen_center_y
+        self.kamera.move_to(spielfigur_centered)
 
     def on_draw(self):
         self.clear()
+        self.kamera.use()
+
+        if self.spielfigur.center_y < 0:
+            arcade.draw_text("LOOSER",self.spielfigur.center_x, 500, arcade.color.BLACK_LEATHER_JACKET, font_size=300,font_name="Kenney Blocks",anchor_x="center",anchor_y="center")
 
         self.szene.draw()
     def on_update(self, deltatime):
         self.spielfigur.update()
         self.physik_engine.update()
+        self.kamera.move_to((self.spielfigur.center_x, self.spielfigur.center_y))
+        self.kamera.update()
+        self.center_camera_to_player()        
+
+        
+
 
 Plattformer()
 arcade.run()
